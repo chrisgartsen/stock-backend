50d36d3be5a12df9360ef48056a05e0f3d087e1d
Chris Gartsen <github@gartsen.nl>
2018-02-01 14:15:10 +0100

    remove password data from return json

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+
+Item.delete_all
+
 Item.create(name: 'Item 1', quantity: 0, minimum_quantity: 2)
 Item.create(name: 'Item 2', quantity: 1, minimum_quantity: 3)
 Item.create(name: 'Item 3', quantity: 3, minimum_quantity: 2)
@@ -7,3 +10,12 @@ Item.create(name: 'Item 6', quantity: 0, minimum_quantity: 0)
 Item.create(name: 'Item 7', quantity: 3, minimum_quantity: 3)
 
 p "Created #{Item.count} Items"
+
+
+User.delete_all
+
+User.create(name: 'Admin', email: 'admin@email.com', admin: true, active: true, password: 'admin')
+User.create(name: 'Guest', email: 'guest@email.com', password: 'guest')
+User.create(name: 'Chris', email: 'chris@email.com', password: 'chrisr')
+
+p "Created #{User.count} Users"
