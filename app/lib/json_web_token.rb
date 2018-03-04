50994e21e605c08a35273f6dd6db2f65c9ed4747
Chris Gartsen <github@gartsen.nl>
2018-03-04 15:48:06 +0100

    Add Heroku support

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -15,5 +15,4 @@ class JsonWebToken
     }
   end
 
-
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'http://localhost:8080','https://stock-8112e.firebaseapp.com/'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
