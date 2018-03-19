84467cefac0b21727e286a40abebca5ebb2f90d8
Chris Gartsen <github@gartsen.nl>
2018-03-19 14:39:10 +0100

    set email validation as post route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :users
   resources :items
 
-  get 'email_validator/:email', to: 'users#unique_email?'
+  post 'email_validator', to: 'users#unique_email?'
   post 'login', to: 'users#login'
+  post 'register', to: 'users#create'
 end
